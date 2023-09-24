8485ece30b465ab5bdf5da06428be8bd21d62b45
tk-pred_ek <114417494+EijiroKubo@users.noreply.github.com>
2023-09-24 13:14:33 +0900

    Add files via upload

--- apps/geodjango/Gmaps/static/map/app.js
+++ apps/geodjango/Gmaps/static/map/app.js
@@ -23,34 +23,60 @@ var baseMaps = {
 
 // 座標とズームレベルを指定
 var map = L.map('map', {layers: [pale]});
-map.setView([35.701037, 139.742553], 10);
-
-// コントロールはオープンにする
-L.control.layers(baseMaps, null, {collapsed:false}).addTo(map);
-
-//スケールコントロールを追加（オプションはフィート単位を非表示）
-L.control.scale({imperial: false}).addTo(map);
+map.setView([35.701037, 139.742553], 16);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     // 右下にクレジットを表示
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
+// ベースマップのコントロールはオープンにする
+L.control.layers(baseMaps, null, {collapsed:false}).addTo(map);
 
-function onMapClick(e) {
-    popup
-        .setLatLng(e.latlng)
-        .setContent("クリック位置 " + e.latlng.toString())
-        .openOn(map);
+
+//スケールコントロールを追加（オプションはフィート単位を非表示）
+//L.control.scale({imperial: false}).addTo(map);
+
+
+function onMapClick(e) { 
+    //popup
+    //    .setLatLng(e.latlng)
+    //    .setContent("クリック位置 " + e.latlng.toString())
+    //    .openOn(map);
+    init()
 }
 
 var popup = L.popup();
 map.on('click', onMapClick);
 
+var bound; // 表示領域（全体表示で表示される範囲）
+var layer; // 地図画像レイヤ
+var input; // 透過度スライダ要素
 
-/* GeoJSONレイヤーを追加方法*/
-$.getJSON("/gmap/geojson_ex/", function(data) {
-    //layer.addData(data);
-    L.geoJson(data).addTo(map);
-});
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+};
+
+async function init() {
+
+	bound = [[35.708251, 139.747811], [35.697982, 139.736108]]; // 地図画像の範囲
+    map.setView([35.7031165, 139.7419595 ], 17);
+
+    for (  var i = 0;  i < 200;  i++  ) {
+        await sleep(80);
+        layer = L.imageOverlay('../../graphs/result_'+ String(i) + '.png', bound, {opacity: '0.8'});
+        layer.addTo(map); // 画像ファイルの重ね合わせ
+        
+        await sleep(1);
+        if ( i > 0 ) { 
+            map.removeLayer( layer_tmp ); 
+        }        
+        
+        layer_tmp = layer  
+    } 
+}
+
+function chgOpacity() {
+	layer.setOpacity(input.value / 100.0); // 透過度変更
+}
 
